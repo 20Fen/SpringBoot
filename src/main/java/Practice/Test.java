2e8a31e84a444a3a9e669cede1e984f970f113a0
Ykw <y15622969639@163.com>
2020-06-08 16:51:20 +0800

    Y

--- src/main/java/Practice/Test.java
+++ src/main/java/Practice/Test.java
@@ -4,6 +4,8 @@ import com.example.demo.system.util.MD5;
 import com.example.demo.system.util.NumberUtil;
 import org.apache.commons.codec.digest.DigestUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -28,6 +30,44 @@ public class Test {
         String twoPlace = NumberUtil.getTwoPlace(a);
         System.out.println(twoPlace);
 
+//    按顺序获取三个数字加一个字母组合
+        System.out.println(getRandom().size());
+        List stringRandom = getStringRandom(getRandom());
+        System.out.print(stringRandom);
+    }
 
+    public static List<String> getRandom() {
+
+        List<String> list = new ArrayList();
+        String b;
+        for (int i = 0; i < 1000; i++) {
+            b = i + "";
+            int length = b.length();
+            if(length == 1){
+                b="00"+i;
+            }
+            if(length == 2){
+                b="0"+i;
+            }
+            list.add(b);
+        }
+        return list;
+    }
+
+    public static List getStringRandom(List<String> list) {
+        List list1=new ArrayList();
+        for (int i = 65; i <= 90; i++) {
+            for (String s : list) {
+                String a3 = s.substring(0, 1);
+                String a2 = s.substring(1, 2);
+                String a1 = s.substring(2, 3);
+
+                list1.add((char)i+""+a3+a2+a1);
+                list1.add(a3+""+(char)i+a2+a1);
+                list1.add(a3+""+a2+(char)i+a1);
+                list1.add(a3+""+a2+a1+(char)i);
+            }
+        }
+        return list1;
     }
 }
